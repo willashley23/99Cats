6081c653ebd273285fdfaeb44cbea59b276d9026
Will Ashley <willashley23@gmail.com>
2016-08-02 14:54:04 -0700

    Add basic styling to spice up our lives. Add edit page to update cats.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -11,6 +11,36 @@ class CatsController < ApplicationController
     @cat = Cat.new
   end
 
+  def create
+    @cat = Cat.new(cat_params)
+      if @cat.save
+        redirect_to cat_url(@cat)
+      else
+        render @cat.errors.full_messages
+      end
+  end
+
+  def edit
+    @cat = Cat.find_by_id(params[:id])
+  end
+
+  def update
+    @cat = Cat.find_by_id(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render @cat.errors.full_messages
+    end
+  end
+
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :sex, :color, :birth_date, :description)
+  end
+
+
 
 
 
