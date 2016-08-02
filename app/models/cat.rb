7b23b189ddfb588344dcfb753409eb4c48d2f8b6
Will Ashley <willashley23@gmail.com>
2016-08-02 11:12:06 -0700

    Finishes basic cat model validations for color, sex, presence, and calculates age.

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,15 @@
 class Cat < ActiveRecord::Base
+  COLORS = %w{Red, Blue, Green, Yellow}
+
+  validates :birth_date, :color, :name, :sex, :description, presence: true
+  validates :color, inclusion: { in: COLORS, message: "No other colors allowed!!!" }
+  validates :sex, inclusion:{ in: %w{M, F}, message: "Sorry, no gender binaries."}
+  def age
+    require 'date'
+    d = Date.new(2016, 8, 2)
+    current_year = d.year
+    cat_year = self.birth_date.year
+    (current_year - cat_year)
+  end
+
 end
