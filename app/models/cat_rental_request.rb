dce9363f21baeb290525041a3fc39dd4d1100d96
Will Ashley <willashley23@gmail.com>
2016-08-02 17:33:04 -0700

    Finish overlapping date validation

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -1,23 +1,30 @@
 class CatRentalRequest < ActiveRecord::Base
   validates :cat_id, :start_date, :end_date, :status, presence: true
   validates :status, inclusion: { in: %w(PENDING APPROVED DENIED), message: "Invalid status" }
-  validate :overlapping_requests
+  validate :overlapping_approved_requests
   belongs_to :cat
 
 # Add a validation that no two APPROVED cat requests for the same cat can overlap in time.
 
   def overlapping_requests
-    start = self.start_date
-    ending = self.end_date
-    self.cat.cat_rental_requests.where("(start_date BETWEEN(:start AND :ending) OR end_date BETWEEN(:start AND :ending)) OR (start_date < :start AND end_date > :ending)",
-      start: start, ending: ending)
-    #   errors[:overlapping_dates] << "Overlapping dates!"
-    #   puts self.errors.full_messages
-    # end
+    # start = self.start_date
+    # ending = self.end_date
+    # first_failure = self.cat.cat_rental_requests.where((self.start_date): start_date..end_date).to_a
+    # second_failure = self.cat.cat_rental_requests.where((self.end_date): start_date..end_date).to_a
+    # # third_failure = self.cat.cat_rental_requests.where('start_date: < start AND end_date: > ending').to_a
+    # (first_failure + second_failure ).empty?
+    CatRentalRequest
+     .where.not(id: self.id)
+     .where(cat_id: cat_id)
+     .where(<<-SQL, start_date: start_date, end_date: end_date)
+        NOT( (start_date > :end_date) OR (end_date < :start_date) )
+     SQL
   end
 
   def overlapping_approved_requests
-
+    unless overlapping_requests.where("status = 'APPROVED'").empty?
+      errors[:bad_request] << "Invalid date range, dates overlap"
+    end
   end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 Cat.create!(birth_date: Date.new(1999, 3, 3), color: "Red", name: "Turbi", sex: "M", description: "fast?" )
 Cat.create!(birth_date: Date.new(2004, 3, 3), color: "Yellow", name: "Lupas", sex: "F", description: "sick?" )
 Cat.create!(birth_date: Date.new(2014, 3, 3), color: "Green", name: "Gromet", sex: "M", description: "googy" )
-CatRentalRequest.create!(cat_id: 1, start_date: Date.new(2016,1,1), end_date: Date.new(2017,1,1))
-CatRentalRequest.create!(cat_id: 1, start_date: Date.new(2016,6,1), end_date: Date.new(2017,6,1))
+CatRentalRequest.create!(cat_id: 1, start_date: Date.new(2014,1,1), end_date: Date.new(2017,6,1), status: "APPROVED")
+CatRentalRequest.create!(cat_id: 1, start_date: Date.new(2011,6,1), end_date: Date.new(2012,6,1), status: "APPROVED")
