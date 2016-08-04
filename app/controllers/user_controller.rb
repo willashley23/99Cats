862fcf3b6d71e7b0995720b43052c4ccc3227b16
willashley23 <willashley23@gmail.com>
2016-08-03 21:02:56 -0700

    Add route resources for users and session, add create, new, mehtods for usercontroller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,25 @@
 class UserController < ApplicationController
+  before_action :require_no_user!
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login_user!(@user)
+      redirect_to cats_url
+    else
+      flash.new[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  private
+
+  def user_params
+    params.requre(:user).permit(:password, :username)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :cats
   resources :cat_rental_requests
+  resource :session
+  resources :users
 end
